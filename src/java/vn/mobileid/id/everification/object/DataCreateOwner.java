8a7318d928f8983dc451d7f24f6058882c697f82
tatkhanhgia <65067602+tatkhanhgia@users.noreply.github.com>
2023-05-12 11:29:44 +0700

    Add files via upload

---
+++ src/java/vn/mobileid/id/everification/object/DataCreateOwner.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.mobileid.id.everification.object;
+
+/**
+ *
+ * @author GiaTK
+ */
+public class DataCreateOwner {
+        public String username;
+    public String email;
+    public String phone;
+    public String pa;
+    public String face_matching;
+    public String typeVerification;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPa() {
+        return pa;
+    }
+
+    public void setPa(String pa) {
+        this.pa = pa;
+    }
+
+    public String getFace_matching() {
+        return face_matching;
+    }
+
+    public void setFace_matching(String face_matching) {
+        this.face_matching = face_matching;
+    }
+
+    public String getTypeVerification() {
+        return typeVerification;
+    }
+
+    public void setTypeVerification(String typeVerification) {
+        this.typeVerification = typeVerification;
+    }
+    
+    
+}
---
+++ src/java/vn/mobileid/id/everification/object/DataGetChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.mobileid.id.everification.object;
+
+/**
+ *
+ * @author GiaTK
+ */
+public class DataGetChallenge {
+
+    public String challenge_type;
+    public String transaction_data;
+    public String username;
+}
